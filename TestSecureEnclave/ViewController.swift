8fd8c6fae246d7d4f20742060955f64646d18cde
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2022-11-01 20:06:54 +0100

    Initial Commit

---
+++ TestSecureEnclave/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestSecureEnclave
+//
+//  Created by Fabio Bombardi on 01/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
